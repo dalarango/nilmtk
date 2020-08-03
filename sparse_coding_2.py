7ddccc1eb062a4c27c4caaf0fba0ab67e927b6a3
dalarango <davidla89@gmail.com>
2020-08-03 10:55:47 +0200

    Adding smoothing

--- sparse_coding_2.py
+++ sparse_coding_2.py
@@ -1,6 +1,7 @@
 from distutils.version import LooseVersion
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.decomposition import SparseCoder, DictionaryLearning
@@ -24,12 +25,33 @@ def ricker_matrix(width, resolution, n_components):
     D /= np.sqrt(np.sum(D ** 2, axis=1))[:, np.newaxis]
     return D
 
+def smooth(x,window_len=11,window='hanning'):
+        if x.ndim != 1:
+                raise ValueError("smooth only accepts 1 dimension arrays.")
+        if x.size < window_len:
+                raise ValueError("Input vector needs to be bigger than window size.")
+        if window_len<3:
+                return x
+        if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+                raise ValueError("Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+        s=np.r_[2*x[0]-x[window_len-1::-1],x,2*x[-1]-x[-1:-window_len:-1]]
+        if window == 'flat': #moving average
+                w=np.ones(window_len,'d')
+        else:  
+                w=eval('np.'+window+'(window_len)')
+        y=np.convolve(w/w.sum(),s,mode='same')
+        return y[window_len:-window_len+1]
+
+
 ## Parameters
 
 resolution = 1024
 subsampling = 3
 width = 100
 n_components = resolution // subsampling
+
+sr_df = pd.read_csv('Dataanalyse_forbruksverdier.csv', sep=';')
+sr_df = sr_df.stack().str.replace(',','.').unstack()
 
 ## Compute dictionary matrix
 D_fixed = ricker_matrix(width=width, resolution=resolution,
@@ -40,10 +62,23 @@ D_multi = np.r_[tuple(ricker_matrix(width=w, resolution=resolution,
                 for w in (10, 50, 100, 500, 1000))]
 
 ### Original signal
-y = np.linspace(0, resolution - 1, resolution)
-first_quarter = y < resolution / 4
-y[first_quarter] = 3.
-y[np.logical_not(first_quarter)] = -1.
+
+#y = np.linspace(0, resolution - 1, resolution)
+#first_quarter = y < resolution / 4
+#y[first_quarter] = 3.
+#y[np.logical_not(first_quarter)] = -1.
+
+### Sin wave signal
+
+#y = np.sin(np.arange(0, 10, 0.009765625))
+#y = y * np.random.normal(loc=1.0, scale=0.10, size=resolution)
+
+### Stig Rune's signal
+
+y = sr_df['VOLUM'].iloc[0:resolution].values
+y = y.astype('float64')
+
+y = smooth(y, window_len=50, window='hanning')
 
 # List the different sparse coding methods in the following format:
 # (title, transform_algorithm, transform_alpha,
@@ -54,8 +89,6 @@ lw = 2
 
 # Avoid FutureWarning about default value change when numpy >= 1.14
 lstsq_rcond = None if LooseVersion(np.__version__) >= '1.14' else -1
-
-
 
 
 D = D_fixed
@@ -92,17 +125,16 @@ squared_error = np.sum((y - z) ** 2)
 
 
 
-plt.plot(y, color= 'black', lw=lw, linestyle='--', label='Original signal')
+plt.plot(y, color= 'black', lw=lw, linestyle='--', label='Original signal', alpha=0.5)
 plt.plot(x, color=color_1, lw=lw,
             label='%s: %s nonzero coefs,\n%.2f error'
-            % (title, density, squared_error))
+            % (title, density, squared_error), alpha=0.5)
 plt.plot(z, color=color_2, lw=lw,
             label='%s: %s nonzero coefs,\n%.2f error'
-            % (title, density, squared_error))
+            % (title, density, squared_error), alpha=0.5)
 plt.axis('tight')
 plt.legend(shadow=False, loc='best')
 plt.show()
-
 
 
 
